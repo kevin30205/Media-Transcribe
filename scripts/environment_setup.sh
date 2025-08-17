4b899257ec8eefb39877e435cc76314e36a6bea9
kevin30205 <kevin30205@gmail.com>
2025-08-17 16:13:01 +0800

    scripts/environment_setup.sh: script for env setup

---
+++ scripts/environment_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# conda create --name Media-Transcribe python=3.10.0 -y
+# conda activate Media-Transcribe
+
+pip install faster-whisper
+conda install -c conda-forge hydra-core -y
+conda install -c conda-forge loguru -y
